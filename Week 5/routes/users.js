63bfb320d2775d493f93c78c5db9fab15e711b81
ardipradiva <ardipradiva16@gmail.com>
2024-03-11 14:58:00 +0700

    Commit

--- Week 5/routes/users.js
+++ Week 5/routes/users.js
@@ -1,9 +1,23 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
+
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const userController = require('../controllers/user.controller')
+
+router.get('/', userController.getAllUsers)
+router.post('/', userController.createUser)
+router.put('/:id', userController.editUser)
+router.delete('/:id', userController.deleteUser)
+router.get('/:id', userController.getDetailUser)
 
 module.exports = router;
